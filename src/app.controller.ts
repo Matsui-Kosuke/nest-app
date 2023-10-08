5af4d380a9111114c9d2a1f5388d6dfe609620fe
Matsui-Kosuke <matsui.kosuke@arsaga.jp>
2023-10-08 10:42:22 +0900

    Prisma ORMでデータ取得

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { UserService } from './user.service';
+import { User as UserModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly userService: UserService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Get('user')
+  async getUser(): Promise<UserModel | null> {
+    return this.userService.user({
+      id: 1,
+    });
   }
 }
